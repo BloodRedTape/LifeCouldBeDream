836a498426cd807d2faa654054f00a62adfe39ee
I.Kostiuk <gefestroyale@gmail.com>
2024-08-27 11:32:38 +0300

    fixes, you know

---
+++ bot/sources/http.hpp
@@ -0,0 +1,132 @@
+#pragma once
+
+#define CPPHTTPLIB_OPENSSL_SUPPORT 1
+#include <httplib.h>
+#include <optional>
+#include <string>
+#include <nlohmann/json.hpp>
+#include <bsl/log.hpp>
+
+DEFINE_LOG_CATEGORY(Http)
+
+inline httplib::Client MakeSecureClient(const std::string& endpoint) {
+	return httplib::Client(endpoint);
+}
+
+inline std::optional<std::string> HttpGet(const std::string& endpoint, const std::string &path, httplib::Headers headers = {}) {
+    httplib::Client client = MakeSecureClient(endpoint);
+
+	auto resp = client.Get(path, headers);
+
+	if (!resp || resp.error() != httplib::Error::Success){
+		LogHttp(Error, "GET Request to % failed with internal error %", endpoint + path, httplib::to_string(resp.error()));
+		return std::nullopt;
+	}
+
+	if (resp->status != httplib::StatusCode::OK_200){
+		LogHttp(Error, "GET Request to % failed with http status %", endpoint + path, resp->status);
+		return std::nullopt;
+	}
+
+	return resp->body;
+}
+
+inline std::optional<httplib::StatusCode> HttpGetStatus(const std::string& endpoint, const std::string &path, httplib::Headers headers = {}) {
+    httplib::Client client = MakeSecureClient(endpoint);
+
+	auto resp = client.Get(path, headers);
+
+	if (!resp || resp.error() != httplib::Error::Success){
+		LogHttp(Error, "GET Request to % failed with internal error %", endpoint + path, httplib::to_string(resp.error()));
+		return std::nullopt;
+	}
+
+	if (resp->status != httplib::StatusCode::OK_200){
+		LogHttp(Error, "GET Request to % failed with http status %", endpoint + path, resp->status);
+		return std::nullopt;
+	}
+
+	return resp->status;
+}
+
+
+inline nlohmann::json HttpGetJson(const std::string& endpoint, const std::string &path, httplib::Headers headers = {}) {
+	auto res = HttpGet(endpoint, path, headers);
+
+	if(!res.has_value())
+		return {};
+
+	return nlohmann::json::parse(res.value(), nullptr, false, false);
+}
+
+inline std::optional<std::string> HttpPost(const std::string& endpoint, const std::string &path, httplib::Headers headers = {}, const std::string &content = {}, const std::string &content_type = "text/plain") {
+    httplib::Client client = MakeSecureClient(endpoint);
+
+	auto resp = content.size() ? client.Post(path, headers, content, content_type) : client.Post(path, headers);
+
+	if (!resp || resp.error() != httplib::Error::Success){
+		LogHttp(Error, "POST Request to % failed with internal error %", endpoint + path, httplib::to_string(resp.error()));
+		return std::nullopt;
+	}
+
+	if (resp->status != httplib::StatusCode::OK_200){
+		LogHttp(Error, "POST Request to % failed with http status %", endpoint + path, resp->status);
+		return std::nullopt;
+	}
+
+	return resp->body;
+}
+
+inline std::optional<httplib::StatusCode> HttpPostStatus(const std::string& endpoint, const std::string &path, httplib::Headers headers = {}, const std::string &content = {}, const std::string &content_type = "text/plain") {
+    httplib::Client client = MakeSecureClient(endpoint);
+
+	auto resp = content.size() ? client.Post(path, headers, content, content_type) : client.Post(path, headers);
+
+	if (!resp || resp.error() != httplib::Error::Success){
+		LogHttp(Error, "POST Request to % failed with internal error %", endpoint + path, httplib::to_string(resp.error()));
+		return std::nullopt;
+	}
+
+	if (resp->status != httplib::StatusCode::OK_200){
+		LogHttp(Error, "POST Request to % failed with http status %", endpoint + path, resp->status);
+		return std::nullopt;
+	}
+
+	return resp->status;
+}
+
+
+inline nlohmann::json HttpPostJson(const std::string& endpoint, const std::string &path) {
+	auto res = HttpPost(endpoint, path);
+
+	if(!res.has_value())
+		return {};
+
+	return nlohmann::json::parse(res.value(), nullptr, false, false);
+}
+
+template<typename T>
+std::optional<T> GetJsonProperty(const std::string &json_string, const std::string &property) {
+	auto json = nlohmann::json::parse(json_string, nullptr, false, false);
+
+	if(!json.count(property))
+		return std::nullopt;
+
+	return json[property].get<T>();
+}
+
+template<typename T>
+std::optional<T> GetJsonObject(const std::string& json_string) {
+	try {
+		T object = nlohmann::json::parse(json_string);
+
+		return object;
+	} catch (...) {
+		return std::nullopt;
+	}
+}
+
+template<typename T>
+T GetJsonPropertyOr(const std::string &json_string, const std::string &property, T value) {
+	return GetJsonProperty<T>(json_string, property).value_or(value);
+}
--- bot/sources/main.cpp
+++ bot/sources/main.cpp
@@ -1,4 +1,4 @@
-#define CPPHTTPLIB_OPENSSL_SUPPORT
+﻿#define CPPHTTPLIB_OPENSSL_SUPPORT
 #include <httplib.h>
 #undef SendMessage
 #include <bsl/log.hpp>
@@ -6,24 +6,31 @@
 #include <INIReader.h>
 #include "simple_bot.hpp"
 #include <tgbot/net/TgLongPoll.h>
-#include <nlohmann/json.hpp>
-
+#include "http.hpp"
 
 const char *Section = "Bot";
-static std::chrono::steady_clock::time_point s_LastUpdate = std::chrono::steady_clock::now();
-static httplib::Server s_Server;
 
 class DreamBot: public SimpleBot{
-	bool m_LastLightStatus = true;
+	std::optional<bool> m_LastLightStatus;
 	std::vector<std::int64_t> m_Chats;
 	const char *ChatsFile = "chats.json";
+
+	std::string m_ServerEndpoint;
+
+	static constexpr const char *Svet = (const char*)u8"🟢Свет есть!🟢";
+	static constexpr const char *NoSvet = (const char*)u8"🔴Свет закончился...🔴";
+
+	static constexpr const char *SvetNotify = (const char*)u8"🟢Свет!🟢";
+	static constexpr const char *NoSvetNotify = (const char*)u8"🔴No Свет?🔴";
 public:
-	DreamBot(const std::string &token):
-		SimpleBot(token)
+	DreamBot(const std::string &token, const std::string &server_endpoint):
+		SimpleBot(token),
+		m_ServerEndpoint(server_endpoint)
 	{
 		OnCommand("light_status", this, &DreamBot::OnStatus);
 		OnCommand("light_enable", this, &DreamBot::OnEnable);
 		OnCommand("light_disable", this, &DreamBot::OnDisable);
+		OnCommand("driver_disconnect", this, &DreamBot::OnDriverDisconnect);
 
 		std::string content = ReadEntireFile(ChatsFile);
 		
@@ -38,19 +45,23 @@ public:
 		auto old_status = m_LastLightStatus;
 		m_LastLightStatus = IsLightPresent();
 
-		if(old_status != m_LastLightStatus)
+		if(old_status.has_value() && m_LastLightStatus.has_value() 
+		&& old_status != m_LastLightStatus)
 			OnUpdate();
 	}
 
 	void OnUpdate() {
+		if(!m_LastLightStatus.has_value())
+			return;
+
 		for(auto chat: m_Chats){
-			SendMessage(chat, 0, m_LastLightStatus ? "Svet?" : "No Svet?");
+			SendMessage(chat, 0, m_LastLightStatus.value() ? SvetNotify : NoSvetNotify);
 			std::this_thread::sleep_for(std::chrono::milliseconds(200));
 		}
 	}
 
 	void OnStatus(TgBot::Message::Ptr message) {
-		ReplyMessage(message, IsLightPresent() ? "Svet." : "No Svet.");
+		ReplyMessage(message, IsLightPresent() ? Svet : NoSvet);
 	}
 	void OnEnable(TgBot::Message::Ptr message) {
 		if(std::count(m_Chats.begin(), m_Chats.end(), message->chat->id))
@@ -64,8 +75,73 @@ public:
 		ReplyMessage(message, "Hehe, undisableable");
 	}
 
+	void OnDriverDisconnect(TgBot::Message::Ptr message) {
+		if(message->from->username != "BloodRedTape")
+			return;
+
+		auto status = HttpPostStatus(m_ServerEndpoint, "/driver/disconnect");
+
+		ReplyMessage(message, status.has_value() ? Format("Status: %", (int)status.value()) : "Failed");
+	}
+
+	std::optional<bool> IsLightPresent()const {
+		auto status = HttpGetStatus(m_ServerEndpoint, "/light/status");
+
+		if(!status.has_value())
+			return std::nullopt;
+
+		if(status.value() == httplib::OK_200)
+			return true;
+		if(status.value() == httplib::Gone_410)
+			return false;
+
+		return std::nullopt;
+	}
+};
+
+
+class DreamServer: public httplib::Server{
+	std::chrono::steady_clock::time_point m_LastUpdate = std::chrono::steady_clock::now();
+	bool m_DriverPresent = false;
+public:
+
+	DreamServer() {
+		Post("/light/status", [&](const httplib::Request& req, httplib::Response& resp) {
+			m_LastUpdate = std::chrono::steady_clock::now();
+		});
+
+		Get ("/light/status", [&](const httplib::Request& req, httplib::Response& resp) {
+			if (!IsDriverPresent()) {
+				resp.status = httplib::StatusCode::NotFound_404;
+				return;
+			}
+
+			resp.status = IsLightPresent() 
+				? httplib::StatusCode::OK_200 
+				: httplib::StatusCode::Gone_410;
+		});
+
+		Get ("/driver/status", [&](const httplib::Request& req, httplib::Response& resp) {
+			resp.status = IsDriverPresent() 
+				? httplib::StatusCode::OK_200 
+				: httplib::StatusCode::Gone_410;
+		});
+
+		Post("/driver/connect", [&](const httplib::Request& req, httplib::Response& resp) {
+			m_LastUpdate = std::chrono::steady_clock::now();
+			m_DriverPresent = true;
+		});
+		Post("/driver/disconnect", [&](const httplib::Request& req, httplib::Response& resp) {
+			m_DriverPresent = false;
+		});
+	}
+
 	bool IsLightPresent()const {
-		return (std::chrono::steady_clock::now() - s_LastUpdate) < std::chrono::seconds(3);
+		return (std::chrono::steady_clock::now() - m_LastUpdate) < std::chrono::seconds(3);
+	}
+
+	bool IsDriverPresent()const {
+		return m_DriverPresent;
 	}
 };
 
@@ -77,30 +153,33 @@ int main() {
 		return EXIT_FAILURE;
 	}
 
+	std::string hostname = config.Get("Server", "Host", "localhost");
+	int port = config.GetInteger("Server", "Post", 1488);
+	std::string token = config.Get("Bot", "Token", "");
 
-	s_Server.Post("/light_status", [](const httplib::Request& req, httplib::Response& resp) {
-		Println("Updated");
-		s_LastUpdate = std::chrono::steady_clock::now();
-	});
-	
+	std::string endpoint = Format("http://%:%", hostname, port);
+
 	std::thread server_thread(
 		[](std::string host, int port){
-			bool status = s_Server.listen(host, port);
+			DreamServer server;
+			bool status = server.listen(host, port);
 
 			Println("Status: %", status);
 		},
-		config.Get(Section, "Host", "localhost"),
-		config.GetInteger(Section, "Post", 1488)
+		hostname,
+		port
 	);
 	
-	DreamBot bot(
-		config.Get(Section, "Token", "")
-	);
+	DreamBot bot(token, endpoint);
 
 
-	TgBot::TgLongPoll poll(bot, 100, 2);
+	TgBot::TgLongPoll poll(bot, 100, 1);
 	while (true) {
-		poll.start();
-		bot.Tick();
+		try{
+			poll.start();
+			bot.Tick();
+		} catch (const std::exception &e) {
+			Println("%", e.what());
+		}
 	}
 }
