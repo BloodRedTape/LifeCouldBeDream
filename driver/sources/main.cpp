17656c344fe142e43bfc1e43b4b665f6498a2cef
I.Kostiuk <gefestroyale@gmail.com>
2024-08-28 13:37:11 +0300

    driver logs time now

--- driver/sources/main.cpp
+++ driver/sources/main.cpp
@@ -2,6 +2,17 @@
 #include <httplib.h>
 #include <bsl/file.hpp>
 #include <bsl/log.hpp>
+#include <chrono>
+
+auto CurrentTime() {
+    auto now = std::chrono::system_clock::now();
+    auto now_time_t = std::chrono::system_clock::to_time_t(now);
+    auto now_tm = *std::localtime(&now_time_t);
+    
+    return std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S");
+}
+
+DEFINE_LOG_CATEGORY(Driver)
 
 int main() {
 	httplib::Client http("http://driver.dream.bloodredtape.com");
@@ -12,7 +23,9 @@ int main() {
 		auto res = http.Post("/driver/light/update");
 		
 		if (!res || res->status != 200) {
-			Println("Failed to update status, %", to_string(res.error()));
+			LogDriver(Error, "[%]: Failed to update status, %", CurrentTime(), to_string(res.error()));
+		} else {
+			LogDriver(Display, "[%]: Updated status", CurrentTime());
 		}
 
 		std::this_thread::sleep_for(Period);
